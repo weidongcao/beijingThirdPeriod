c7287c0f3a733883f7908f10022fdb35e5d997d1
yunchen <caowd1990@gmail.com>
2017-09-05 16:15:54 +0800

    修改BCP文件导入Solr的执行脚本，修改Solrj的Demo

--- src/main/java/com/rainsoft/j2se/TestSolr.java
+++ src/main/java/com/rainsoft/j2se/TestSolr.java
@@ -1,38 +1,136 @@
 package com.rainsoft.j2se;
 
-import com.rainsoft.domain.RegContentFtp;
-import com.rainsoft.utils.ReflectUtils;
-import com.rainsoft.utils.SolrUtil;
+import com.rainsoft.FieldConstants;
+import com.rainsoft.conf.ConfigurationManager;
+import com.rainsoft.utils.DateUtils;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Administrator on 2017-06-21.
  */
 public class TestSolr {
-    public static void main(String[] args) throws IOException, SolrServerException {
-        CloudSolrClient client = (CloudSolrClient) SolrUtil.getSolrClient(true);
-        SolrInputDocument doc = new SolrInputDocument();
-        RegContentFtp ftp = new RegContentFtp();
-        ftp.setId("abcdefg");
-        ftp.setCapture_time("2017-06-21 16:24:05");
-        ftp.setAccount("test");
+    public static CloudSolrClient client;
 
-        Field[] fields = RegContentFtp.class.getFields();
+    static {
+        //Zookeeper节点主机名
+        String zkHost = "dn1.hadoop.com," +
+                "dn2.hadoop.com," +
+                "dn3.hadoop.com," +
+                "dn4.hadoop.com," +
+                "nn2.hadoop.com";
+        //通过Zookeeper获取Solr连接
+        client = new CloudSolrClient
+                .Builder()
+                .withZkHost(zkHost)
+                .build();
+        //指定要连接的Solr的collection
+        client.setDefaultCollection(
+                ConfigurationManager
+                        .getProperty("solr_collection")
+        );
+    }
 
-        for (Field field : fields) {
-            String fieldName = field.getName();
-            String fieldValue = (String) ReflectUtils.getFieldValueByName(fieldName, ftp);
-//            System.out.println(fieldName + " = " + fieldValue);
-            doc.addField(fieldName.toUpperCase(), fieldValue);
-        }
+    public static void main(String[] args)
+            throws IOException, SolrServerException {
+        //从Solr查询数据
+        querySolr(
+                "docType:\"网页\" " +
+                "USER_NAME:\"郭晓静\" " +
+                "REF_DOMAIN:\"speed.qq.com\" " +
+                "DEST_IP:\"3722762918\""
+        );
+        //向Solr插入或者更新索引的例子
+        insertOrupdateSolr();
 
-        client.add(doc);
-
+        //删除Solr的索引
+        deleteByQuery(
+                "docType:\"网页\" " +
+                "USER_NAME:\"郭晓静\" " +
+                "REF_DOMAIN:\"speed.qq.com\" " +
+                "DEST_IP:\"3722762918\""
+        );
+        // 关闭连接
         client.close();
+    }
+
+    public static void insertOrupdateSolr()
+            throws IOException, SolrServerException {
+        //当前时间
+        Date curDate = new Date();
+        //生成要插入Solr的数据
+        List<SolrInputDocument> docs = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            String rowKey = UUID.randomUUID()
+                    .toString().replace("-", "")
+                    + "_" + curDate.getTime();
+            String captureTimeMinSecond = rowKey.split("_")[0];
+            String id = rowKey.split("_")[1];
+            SolrInputDocument doc = new SolrInputDocument();
+            //仅有的字段
+            doc.addField("ID".toUpperCase(), id);
+            doc.addField("SID".toUpperCase(), rowKey);
+            doc.addField("docType", "网页");
+            doc.addField(
+                    "IMPORT_TIME".toUpperCase()
+                    , DateUtils.TIME_FORMAT.format(curDate)
+            );
+            doc.addField("import_time", curDate.getTime());
+            doc.addField(
+                    "capture_time".toLowerCase()
+                    , Long.valueOf(captureTimeMinSecond)
+            );
+            //独有的字段
+            //TODO
+            docs.add(doc);
+        }
+        //在1分钟内自动提交到Solr
+        client.add(docs, 1000);
+    }
+
+    public static void deleteByQuery(String query) {
+        try {
+            client.deleteByQuery(query);
+            client.commit();
+        } catch (SolrServerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void querySolr(String q)
+            throws IOException, SolrServerException {
+        SolrQuery params = new SolrQuery();
+        params.set("q", q);
+        params.set("start", 0);
+        params.set("rows", 5);
+        QueryResponse rsp = client.query(params);
+        SolrDocumentList docs = rsp.getResults();
+        System.out.println("文档数量：" + docs.getNumFound());
+        System.out.println("------query data:------");
+        for (SolrDocument doc : docs) {
+            // 多值查询
+            @SuppressWarnings("unchecked")
+            String ID = (String) doc.getFieldValue("ID");
+            String SID = (String) doc.getFieldValue("SID");
+            String subject = (String)
+                    doc.getFieldValue("SUBJECT");
+            System.out.println("ID:" + ID
+                    + "\t SID:"
+                    + SID + "\t SUBJECT" + subject);
+        }
+        System.out.println("-----------------------");
     }
 }
--- src/main/scripts/startup-bcp_to_hbase_solr.sh
+++ src/main/scripts/startup-bcp_to_hbase_solr.sh
@@ -1,19 +1,123 @@
-#!/usr/bin/env bash
+#!/bin/bash
+## 将BCp文件的数据导入到Solr和HBase
 
-for bcp in http bbs email search ftp weibo im_chat service_info
+
+# BCP文件转TSV
+bcp_transform_tsv_cmd_template="java -classpath BeiJingThirdPeriod.jar com.rainsoft.bcp.TransformBcp2Tsv"
+
+# TSV文件导入HBase
+bcp_import_hbase_cmd_template="spark-submit --master local[4] --driver-memory 16g --executor-memory 4g --class com.rainsoft.bcp.Main /opt/modules/BeiJingThirdPeriod/BeiJingThirdPeriod.jar"
+# TSV 导入Solr
+bcp_import_solr_cmd_template="java -classpath BeiJingThirdPeriod.jar com.rainsoft.bcp.BcpToSolr"
+
+# 打印日志
+function logger() {
+    # 写入日志文件
+    echo "[`date "+%Y-%m-%d %H:%M:%S"`  INFO] ${1}" >> logs/bcp_to_hbase_solr-`date "+%Y%m%d"`.log
+    #echo "[`date "+%Y-%m-%d %H:%M:%S"`  INFO] ${1}"
+}
+
+# 第一个参数：命令
+# 第二个参数：命令的参数
+function run_cmd(){
+    cmdStartTime=`date +%s`
+    logger "要执行的命令：${1} ${2}"
+    ${1} ${2} >> logs/bcp_to_hbase_solr-`date "+%Y%m%d"`.log
+    #echo "${1} ${2}"
+    cmdRunResultStatus=$?
+    cmdEndTime=`date +%s`
+    if [[ ${cmdRunResultStatus} -eq 0 ]]; then
+        logger "命令执行完成,耗时: $(((${cmdEndTime} - ${cmdStartTime})/60)) 分钟, $(((${cmdEndTime} - ${cmdStartTime})%60)) 秒"
+    else
+        logger "命令执行失败,耗时: $(((${cmdEndTime} - ${cmdStartTime})/60)) 分钟, $(((${cmdEndTime} - ${cmdStartTime})%60)) 秒"
+		logger "休眠一个小时..."
+        # 程序休眠一个小时
+        sleep 1h
+    fi
+    return ${cmdRunResultStatus}
+}
+
+# 执行数据导入(导入HBase和Solr)
+# 参数说明
+# 就一个参数:task类型(ftp, im_chat, http)
+function run_import() {
+    # BCP数据导入到HBase
+    run_cmd "${bcp_import_hbase_cmd_template}" "${1}"
+
+    # BCP数据索引到Solr
+    run_cmd "${bcp_import_solr_cmd_template}" "${1}"
+    # 判断BCP数据索引到Solr是否成功完成,如果成功完成的话删除本地TSV文件
+    if [[ $? -eq 0 ]]; then
+      #删除TSV文件
+      rm -f /opt/modules/BeiJingThirdPeriod/oracle_workspace/work/bcp-${1}/*
+      logger "删除TSV文件完成: ${1}"
+      # 删除BCP文件
+      rm -f /opt/bcp/${1}/*
+      logger "删除BCP文件完成: ${1}"
+    fi
+}
+
+rm -f /opt/bcp/ftp/*
+rm -f /opt/bcp/im_chat/*
+rm -f /opt/bcp/http/*
+
+while [[ 1 = 1 ]];
 do
-	bcppath=/opt/bcp/$bcp
-	if [ -z "`ls ${bcppath}`" ];then
-		echo "mv -f /opt/bcp/*-$bcp*.bcp ${bcppath}"
-                mv -f /opt/bcp/*-$bcp*.bcp  ${bcppath}
-	else
-		echo "$bcppath is not empty."
-	fi
+    # job start time
+    jobStartTime=`date +%s`
+    for task in im_chat ftp http
+    do
+        bcppath=/opt/bcp/${task}
+
+        # 判断TSV文件工作目录下有没有文件，如果有文件，先处理这些文件
+        tsvFileCount=`ls /opt/modules/BeiJingThirdPeriod/oracle_workspace/work/bcp-${task} | wc -l`
+        if [[ ${tsvFileCount} -gt 0 ]]; then
+            # TSV数据导入到HBase和Solr
+            logger "有未处理完成的TSV数据文件,类型为: ${task}.先处理此TSV文件..."
+            run_import "${task}"
+        fi
+
+        # 再次判断TSV文件工作目录下有没有文件，如果没有文件，再处理BCP文件
+        tsvFileCount=`ls /opt/modules/BeiJingThirdPeriod/oracle_workspace/work/bcp-${task} | wc -l`
+
+        # 如果TSV工作目录没有tsv文件且BCP文件池内同一类型的文件大于40个再进行处理，否则等待
+        if [[ ${tsvFileCount} -eq 0 ]]; then
+            # 统计数据池内有多少个此类型的BCP文件
+            bcpFileCount=`find /rsdata/out -name "*-${task}*.bcp" | wc -l`
+            if [[ ${bcpFileCount} -gt 40 ]]; then
+				logger "开始处理${task}类型的BCP文件..."
+                # task start time
+                taskStartTime=`date +%s`
+
+                logger "find /rsdata/out -name "*-${task}*.bcp"  | tail -n 200 |xargs -i mv {} ${bcppath}"
+                find /rsdata/out -name "*-${task}*.bcp"  | tail -n 200 |xargs -i mv {} ${bcppath}
+                logger "已将 ${task} 数据文件从数据文件池移动到 ${bcppath}"
+
+                # BCP文件转TSV文件
+                run_cmd "${bcp_transform_tsv_cmd_template}" "${task}"
+
+                # TSV数据导入到HBase和Solr
+                run_import "${task}"
+
+                # task end time
+                taskEndTime=`date +%s`
+                logger "<-------- ${task} 任务处理完成, 耗时: $(((${taskEndTime} - ${taskStartTime})/60)) 分钟, $(((${taskEndTime} - ${taskStartTime})%60)) 秒 -------->"
+            else
+                ## 数据文件池内的文件太少，继续等待
+                logger "数据文件池内的 ${task} 文件数量为:${bcpFileCount},少于40个,继续等待..."
+            fi
+        else
+            # TSV文件没有处理成功
+            logger "有未处理的TSV文件,等待TSV文件处理完成再处理BCP文件"
+        fi
+        sleep 1s
+    done
+    # job end time
+    jobEndTime=`date +%s`
+    logger ""
+
+    logger "###########################################################"
+    logger "############## 一次job处理完成，$(((${jobEndTime} - ${jobStartTime})/60)) 分钟, $(((${jobEndTime} - ${jobStartTime})%60)) 秒 ##############"
+    logger "###########################################################"
+    sleep 20s
 done
-
-for job_type in ftp im_chat http
-do
-    /opt/modules/spark/bin/spark-submit --master local[4] --executor-memory 32g --executor-cores 2 --num-executors 2 --class com.rainsoft.bcp.Main /opt/modules/BeiJingThirdPeriod/BeiJingThirdPeriod.jar ${job_type}
-done
-
-
