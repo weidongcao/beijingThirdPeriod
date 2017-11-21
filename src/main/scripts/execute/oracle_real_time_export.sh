a87cfab307a3284fbfee1b62ad7a00d156c58bda
yunchen <caowd1990@gmail.com>
2017-11-21 10:24:31 +0800

    执行脚本优化

--- src/main/scripts/execute/oracle_real_time_export.sh
+++ src/main/scripts/execute/oracle_real_time_export.sh
@@ -5,24 +5,53 @@
 # 跳转到程序的根目录
 cd /opt/modules/BeiJingThirdPeriod/
 
-# 执行程序
-# 跑FTP、IM_CHAT、HTTP三个大表
-# java -classpath BeiJingThirdPeriod.jar com.rainsoft.solr.RunOracleBigTableExport
+javaCmd="java -classpath BeiJingThirdPeriod.jar "
+javaPachage="com.rainsoft.solr"
+javaClass="RunOracleAllTableExport"
 
-# 跑所有的内容表包括:ftp、im_chat,http, bbs,email,search,weibo,real,vid
-java -classpath BeiJingThirdPeriod.jar com.rainsoft.solr.RunOracleAllTableExport
+cmd="${javaCmd} ${javaPachage}.${javaClass}"
 
-# 如果退出的话判断程序是否结束
-while [[ true ]];
-do
-	# 查看进程是否存在,存在的话获取进程PID
-	exit_status=`ps -ef | grep ${0} | grep -v grep | awk '{print $2}'`
-	if [ ! ${exit_status} ];then
-		echo "进程已结束，程序退出"
-		break;
-	else
-		kill -9 ${exit_status}
+## 杀掉进程
+function killProcess() {
+	cmdRunResultStatus=""
+	# 如果退出的话判断程序是否结束
+	while [ ${cmdRunResultStatus} -ne 0 ];
+	do
+		# 查看进程是否存在,存在的话获取进程PID
+		exit_status=`ps -ef | grep ${1} | grep -v grep | awk '{print $2}'`
+		if [ -z ${exit_status} ];then
+			echo "进程已结束，程序退出"
+			break;
+		else
+			kill -9 ${exit_status}
+			cmdRunResultStatus=$?
+		fi
+	done
+
+	return ${cmdRunResultStatus}
+}
+
+## 结束程序
+function exitProgram() {
+	exitStatus=$(killProcess $0)
+	if [ ${exitStatus} -eq 0 ]; then
+		killProcess ${javaClass}
 	fi
-done
+}
 
+startStop=${1}
+if [ -z "${startStop}"]; then
+	startStop="start"
+fi
 
+# 启动或者停止脚本,如果没有参数的话默认开始执行导入
+if [[ "start" == "${startStop}" ]]; then
+	# 执行程序
+	# 跑FTP、IM_CHAT、HTTP三个大表
+	# java -classpath BeiJingThirdPeriod.jar com.rainsoft.solr.RunOracleBigTableExport
+
+	# 跑所有的内容表包括:ftp、im_chat,http, bbs,email,search,weibo,real,vid
+	`${cmd}`
+elif [[ "stop" == "${startStop}" ]]; then
+	exitProgram
+fi
