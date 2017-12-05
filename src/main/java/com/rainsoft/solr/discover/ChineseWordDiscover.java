db16e25190d6cab409a8a5ac0db3ae46db52f319
yunchen <caowd1990@gmail.com>
2017-12-05 16:47:51 +0800

    Solr新词发现算法

---
+++ src/main/java/com/rainsoft/solr/discover/ChineseWordDiscover.java
@@ -0,0 +1,19 @@
+package com.rainsoft.solr.discover;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+
+import java.io.StringReader;
+
+/**
+ * 中文新词发现
+ * Created by CaoWeiDong on 2017-11-15.
+ */
+public class ChineseWordDiscover {
+    public static void main(String[] args) {
+
+    }
+}
+
