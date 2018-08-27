1fb3d62da60bd6674078313e3871ac9afffaa652
yunchen <caowd1990@gmail.com>
2018-08-27 16:05:36 +0800

    演示如何将数据从一个通道复制到另一个通道或从一个文件复制到另一个文件

---
+++ src/main/java/com/rainsoft/j2se/ChannelDemo.java
@@ -0,0 +1,38 @@
+package com.rainsoft.j2se;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+
+/**
+ * 演示如何将数据从一个通道复制到另一个通道或从一个文件复制到另一个文件
+ * Created by CaoWeiDong on 2018-08-27.
+ */
+public class ChannelDemo {
+    public static void main(String[] args) throws IOException {
+        String path = System.getProperty("user.dir");
+        System.out.println("path = " + path);
+        FileInputStream input = new FileInputStream(path + "/testin.txt");
+        ReadableByteChannel source = input.getChannel();
+        FileOutputStream output = new FileOutputStream(path + "/testout.txt");
+        WritableByteChannel destrination = output.getChannel();
+        copyData(source, destrination);
+        source.close();
+        destrination.close();
+        System.out.println("Copy Data finished...");
+    }
+
+    private static void copyData(ReadableByteChannel src, WritableByteChannel dest) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocateDirect(20 * 1024);
+        while (src.read(buffer) != -1) {
+            buffer.flip();
+            while (buffer.hasRemaining()) {
+                dest.write(buffer);
+            }
+            buffer.clear();
+        }
+    }
+}
